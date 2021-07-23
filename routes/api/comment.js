c0f1ac2799f84dbbde0dfdac91a5706b9662d35b
Mikey J <mikeylashaun@icloud.com>
2021-07-22 22:20:29 -0700

    I am added the controllers for the comments

---
+++ routes/api/comment.js
@@ -0,0 +1,17 @@
+const router = require("express").Router(); 
+const CommentController = require("../../controllers/CommentController"); 
+
+// api/comment
+router
+    .route("/")
+    .get(CommentController.findAll)
+    .post(CommentController.create); 
+
+// api/comment/:id 
+router
+    .route("/:id")
+    .get(CommentController.findById)
+    .delete(CommentController.remove)
+
+
+module.exports = router; 
