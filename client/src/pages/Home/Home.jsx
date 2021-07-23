3d425b53dd8abbd0fe40c453e92a726d1baf78b2
Alys Scorpio <alyssad1182@gmail.com>
2021-07-23 13:52:08 -0700

    Resolving conflicts

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -5,10 +5,6 @@ import API from "../../utils/axios/API";
 import './Home.css';
 
 
-<<<<<<< HEAD:client/src/pages/Home/Home.js
-=======
-
->>>>>>> bca96a3d7dea88119ab3125cd9b2662f015a9b4d:client/src/pages/Home/Home.jsx
 const Home = () => {
     return (
         <div>
