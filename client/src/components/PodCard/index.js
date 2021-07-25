fd1517a9b820ce6c7f02ddbb85f2a7558534f652
Alys Scorpio <alyssad1182@gmail.com>
2021-07-24 21:06:46 -0700

    Adding rounded edges to image on card

--- client/src/components/PodCard/index.js
+++ client/src/components/PodCard/index.js
@@ -7,11 +7,11 @@ import { Link } from "react-router-dom";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function PodCard() {
-  return (
+    return (
         <Card 
         className="card"
         style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Img variant="top" src="holder.js/100px180" rounded/>
             <Card.Body>
                 <Card.Title>Card Title</Card.Title>
                 <Card.Text>
